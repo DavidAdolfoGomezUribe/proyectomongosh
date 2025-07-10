ea61d158d6e9ba61c1d93cb3e284ad80d58531b9
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-07-10 10:07:38 -0500

    ok

--- test_dataset.js
+++ test_dataset.js
@@ -346,7 +346,7 @@ db.vehiculos.insertMany([
       ]
     },
     {
-      _id:3,
+      _id: 3,
       nombre: "Bogotá",
       direccion: "Av. Caracas #15-40, Bogotá, Cundinamarca",
       empleados: [1, 7, 8, 9],
@@ -354,40 +354,60 @@ db.vehiculos.insertMany([
         {
           _id: 11,
           tipo_zona: "carros",
+          precio_hora: 2800,
           cupos_vehiculos: [
-            3, 8, 13, 18, 23, 30
-          ],
-          precio_hora: 2800
+            { categoria: "carro", placa: "CRR113", cliente: { nombre: "Andrés Castro", cedula: "2234567891" } },
+            { categoria: "carro", placa: "CRR117", cliente: { nombre: "Sebastián Ramírez", cedula: "2234567893" } },
+            { categoria: "carro", placa: "CRR121", cliente: { nombre: "Tomás Navarro", cedula: "2234567896" } },
+            { categoria: "carro", placa: "CRR125", cliente: { nombre: "Juan Esteban", cedula: "2234567898" } },
+            { categoria: "carro", placa: "CRR129", cliente: { nombre: "Mariana Cruz", cedula: "2234567801" } },
+            { categoria: "moto",  placa: "MTO134", cliente: { nombre: "Santiago Paredes", cedula: "2234567804" } }
+          ]
         },
         {
           _id: 12,
           tipo_zona: "motos",
+          precio_hora: 1800,
           cupos_vehiculos: [
-            5, 10, 15, 20, 25, 29
-          ],
-          precio_hora: 1800
+            { categoria: "moto", placa: "MTO114", cliente: { nombre: "Valentina Mejía", cedula: "2234567892" } },
+            { categoria: "moto", placa: "MTO118", cliente: { nombre: "Isabella Torres", cedula: "2234567894" } },
+            { categoria: "moto", placa: "MTO122", cliente: { nombre: "Lucía Salazar", cedula: "2234567897" } },
+            { categoria: "moto", placa: "MTO126", cliente: { nombre: "Gabriela Cárdenas", cedula: "2234567899" } },
+            { categoria: "moto", placa: "MTO130", cliente: { nombre: "Samuel Duarte", cedula: "2234567802" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Santiago Paredes", cedula: "2234567804" } }
+          ]
         },
         {
           _id: 13,
-          tipo_zona: "ciclas",
+          tipo_zona: "motos",
+          precio_hora: 700,
           cupos_vehiculos: [
-            4, 9, 14, 19, 24, 28
-          ],
-          precio_hora: 700
+            { categoria: "cicla", placa: null, cliente: { nombre: "Andrés Castro", cedula: "2234567891" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Isabella Torres", cedula: "2234567894" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Tomás Navarro", cedula: "2234567896" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Gabriela Cárdenas", cedula: "2234567899" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Mariana Cruz", cedula: "2234567801" } },
+            { categoria: "carro", placa: "CRR133", cliente: { nombre: "Nicole Mendoza", cedula: "2234567803" } }
+          ]
         },
         {
           _id: 14,
           tipo_zona: "camiones",
+          precio_hora: 4500,
           cupos_vehiculos: [
-            2, 7, 12, 17, 22, 29
-          ],
-          precio_hora: 4500
+            { categoria: "camion", placa: "CMN112", cliente: { nombre: "Sofía García", cedula: "2234567890" } },
+            { categoria: "camion", placa: "CMN116", cliente: { nombre: "Sebastián Ramírez", cedula: "2234567893" } },
+            { categoria: "camion", placa: "CMN120", cliente: { nombre: "Daniela Herrera", cedula: "2234567895" } },
+            { categoria: "camion", placa: "CMN124", cliente: { nombre: "Juan Esteban", cedula: "2234567898" } },
+            { categoria: "camion", placa: "CMN128", cliente: { nombre: "Miguel Ángel", cedula: "2234567800" } },
+            { categoria: "cicla", placa: null, cliente: { nombre: "Santiago Paredes", cedula: "2234567804" } }
+          ]
         },
         {
           _id: 15,
           tipo_zona: "empleados",
-          cupos_vehiculos: [],
-          precio_hora: 0
+          precio_hora: 0,
+          cupos_vehiculos: []
         }
       ]
     }
@@ -397,8 +417,8 @@ db.vehiculos.insertMany([
 db.parqueos.insertMany([
   {
     _id: ObjectId("83fc35d32016422e8772da45"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 3 },
     hora_entrada: ISODate("2025-06-26T11:33:19"),
     hora_salida: null,
@@ -407,8 +427,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("de4f8e1b6c4544a29612c7cc"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 8 },
     hora_entrada: ISODate("2025-06-05T17:21:44"),
     hora_salida: null,
@@ -417,8 +437,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("da09abe8d023446da8e2033e"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 13 },
     hora_entrada: ISODate("2025-06-06T21:04:15"),
     hora_salida: null,
@@ -427,8 +447,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("424958cea0244b06a4cf8f46"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 18 },
     hora_entrada: ISODate("2025-06-02T09:40:13"),
     hora_salida: null,
@@ -437,8 +457,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("64a16c04027044b8b612f1bf"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 23 },
     hora_entrada: ISODate("2025-06-11T11:02:00"),
     hora_salida: null,
@@ -447,8 +467,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("a64ce8ff39b349fd88a7c6e7"),
-    sede: 3,
-    zona: 11,
+    sede: "Bogotá",
+    zona: "carros",
     vehiculo: { _id: 30 },
     hora_entrada: ISODate("2025-06-12T01:53:52"),
     hora_salida: null,
@@ -457,8 +477,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("b0a63154b26847f2b5477431"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 5 },
     hora_entrada: ISODate("2025-06-17T11:37:25"),
     hora_salida: null,
@@ -467,8 +487,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("cc5246ffe94343dc9bd27e43"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 10 },
     hora_entrada: ISODate("2025-06-02T13:35:43"),
     hora_salida: null,
@@ -477,8 +497,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("1fcaabf925664087bb2fc4d3"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 15 },
     hora_entrada: ISODate("2025-06-04T19:44:20"),
     hora_salida: null,
@@ -487,8 +507,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("87573762c6354757a71a8f49"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 20 },
     hora_entrada: ISODate("2025-06-26T23:32:19"),
     hora_salida: null,
@@ -497,8 +517,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("83c72a3f6595421b99962e53"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 25 },
     hora_entrada: ISODate("2025-06-19T02:28:04"),
     hora_salida: null,
@@ -507,8 +527,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("34afc4a847b941a5a050278c"),
-    sede: 3,
-    zona: 12,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 29 },
     hora_entrada: ISODate("2025-06-01T05:08:05"),
     hora_salida: null,
@@ -517,8 +537,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("4de9ca58196846048458d06f"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 4 },
     hora_entrada: ISODate("2025-06-27T09:40:32"),
     hora_salida: null,
@@ -527,8 +547,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("13bf9f697fee42dbb6568120"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 9 },
     hora_entrada: ISODate("2025-06-06T16:58:56"),
     hora_salida: null,
@@ -537,8 +557,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("131ace09fbf84fcd8bd7419f"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 14 },
     hora_entrada: ISODate("2025-06-25T10:01:30"),
     hora_salida: null,
@@ -547,8 +567,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("f7ca543139d5493aa9177cae"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 19 },
     hora_entrada: ISODate("2025-06-19T05:40:22"),
     hora_salida: null,
@@ -557,8 +577,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("dc433120e05442839fb5b83c"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 24 },
     hora_entrada: ISODate("2025-06-08T12:03:28"),
     hora_salida: null,
@@ -567,8 +587,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("471c5388e5ea4ad4a8f2e3c4"),
-    sede: 3,
-    zona: 13,
+    sede: "Bogotá",
+    zona: "motos",
     vehiculo: { _id: 28 },
     hora_entrada: ISODate("2025-06-16T01:03:13"),
     hora_salida: null,
@@ -577,8 +597,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("4b29b3b23149432080814834"),
-    sede: 3,
-    zona: 14,
+    sede: "Bogotá",
+    zona: "camiones",
     vehiculo: { _id: 2 },
     hora_entrada: ISODate("2025-06-06T10:31:18"),
     hora_salida: null,
@@ -587,8 +607,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("cbd0eb2444c8492984964aff"),
-    sede: 3,
-    zona: 14,
+    sede: "Bogotá",
+    zona: "camiones",
     vehiculo: { _id: 7 },
     hora_entrada: ISODate("2025-06-25T14:43:37"),
     hora_salida: null,
@@ -597,8 +617,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("0be33676f0da4b27b9eefa8a"),
-    sede: 3,
-    zona: 14,
+    sede: "Bogotá",
+    zona: "camiones",
     vehiculo: { _id: 12 },
     hora_entrada: ISODate("2025-06-01T15:01:16"),
     hora_salida: null,
@@ -607,8 +627,8 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("6454420dfb754eeaa9735fd2"),
-    sede: 3,
-    zona: 14,
+    sede: "Bogotá",
+    zona: "camiones",
     vehiculo: { _id: 17 },
     hora_entrada: ISODate("2025-06-12T18:15:53"),
     hora_salida: null,
@@ -617,82 +637,13 @@ db.parqueos.insertMany([
   },
   {
     _id: ObjectId("09f4bad31e544628acc3bd36"),
-    sede: 3,
-    zona: 14,
+    sede: "Bogotá",
+    zona: "camiones",
     vehiculo: { _id: 22 },
     hora_entrada: ISODate("2025-06-05T19:41:11"),
     hora_salida: null,
     total_pago: 0.0,
     estado: "activo"
-  },
-  {
-    _id: ObjectId("4e59b61ffc594464bcf2a1f5"),
-    sede: 1,
-    zona: 4,
-    vehiculo: { _id: 16 },
-    hora_entrada: ISODate("2025-06-02T22:16:29"),
-    hora_salida: ISODate("2025-06-03T19:16:29"),
-    total_pago: 4463.42,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("55de2e26ae164199a5062d69"),
-    sede: 1,
-    zona: 1,
-    vehiculo: { _id: 21 },
-    hora_entrada: ISODate("2025-06-16T19:28:23"),
-    hora_salida: ISODate("2025-06-19T15:28:23"),
-    total_pago: 5383.62,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("6cae5fb0c212492b8e4bbba0"),
-    sede: 3,
-    zona: 13,
-    vehiculo: { _id: 1 },
-    hora_entrada: ISODate("2025-06-22T01:28:17"),
-    hora_salida: ISODate("2025-06-22T07:28:17"),
-    total_pago: 4638.53,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("df6c19b37a66465fb894baf4"),
-    sede: 1,
-    zona: 2,
-    vehiculo: { _id: 27 },
-    hora_entrada: ISODate("2025-06-20T21:36:54"),
-    hora_salida: ISODate("2025-06-22T21:36:54"),
-    total_pago: 3693.3,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("945f97fa0c464e9c953dd33b"),
-    sede: 2,
-    zona: 8,
-    vehiculo: { _id: 11 },
-    hora_entrada: ISODate("2025-06-01T14:24:28"),
-    hora_salida: ISODate("2025-06-03T07:24:28"),
-    total_pago: 3339.32,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("bc62cdaddb004c2d999e3fe7"),
-    sede: 2,
-    zona: 6,
-    vehiculo: { _id: 6 },
-    hora_entrada: ISODate("2025-06-01T13:43:49"),
-    hora_salida: ISODate("2025-06-02T11:43:49"),
-    total_pago: 6297.98,
-    estado: "finalizado"
-  },
-  {
-    _id: ObjectId("e65c6371c0ce4a77a3764a52"),
-    sede: 3,
-    zona: 13,
-    vehiculo: { _id: 26 },
-    hora_entrada: ISODate("2025-06-14T02:46:20"),
-    hora_salida: ISODate("2025-06-14T06:46:20"),
-    total_pago: 6005.24,
-    estado: "finalizado"
   }
+ 
 ]);
